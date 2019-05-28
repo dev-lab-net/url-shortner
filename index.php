70ca39dfe21a28a80f786b3e881b1d8c6542089f
ErnestBidouille <theo.chennebault@gmail.com>
2019-05-29 00:15:44 +0200

    Url shornet done

--- index.php
+++ index.php
@@ -30,32 +30,31 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 $regex_url = '(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})';
-//$conn->query("Insert into shorttable(url,hash) values ('www.google.com','hash')");
-// var_dump($_POST);
 echo "\n <br>";
-if ($_POST['shortme'] ){
+if ($_POST['shortme']){
         if ($url = trim($_POST['url'])) {
             if(preg_match($regex_url,$url)){
                 $tochange = array( "https://www.","https://", "http://www.", "http://");
                 $url=str_replace($tochange,'www.',$url);
-                if(gethostbyname($url)!="82.216.111.26"){
-                    //echo gethostbyname($url);
                 $res = $conn->query("Select hash from shorttable where url ='".$url."'");
                 $res = $res->fetch_assoc();
-                // var_dump($res);
                 if ($res){
-                    // echo $res['hash'];
                     echo "Partagez le lien <a href='https://go.dev-lab.net/".$res['hash']. "'>go.dev-lab.net/".$res['hash']."</a> :) ";
                 } else {
                     $fullhash = sha1($url);
-                    // echo $fullhash;
                     $hash=substr($fullhash,0,6);
+                    $res = $conn->query("Select 'x' from shorttable where hash ='".$hash."'");
+                    $res = $res->fetch_assoc();
+                    if($res){
+                        $hash = substr($fullhash,0,7);
+                    }
                     $conn->query("Insert into shorttable(url,hash,fullhash) values ('".$url."','".$hash."','".$fullhash."')");
-                    echo "Partagez le lien <a href='https://go.dev-lab.net/".$fullhash. "'>go.dev-lab.net/".$fullhash."</a> :) ";
+                    if(!$conn->connect_error){
+                        echo "<div>Partagez le lien <a href='https://go.dev-lab.net/".$hash. "'>go.dev-lab.net/".$hash."</a> :) </div>";
+                    } else {
+                        echo "<div>Internal error, try again please</div>";
+                    }
                 }
-            }else {
-                echo "L'URL ne renvoie pas vers une IP connue.";
-            }
             }
             else {
                 echo 'Entrez une URL au format "http://(www).exemple.com" ou "https://(www).exemple.com" ou "www.exemple.com"';
@@ -65,6 +64,7 @@ if ($_POST['shortme'] ){
             echo "Entrez une URL";
         }
     }
+    mysqli_close($conn);
     ?>
     </body>
 </html>
---
+++ redirect.php
@@ -0,0 +1,28 @@
+<?php
+$hash =  "{$_SERVER['REQUEST_URI']}";
+try {
+    $res = $conn->query("Select url from shorttable where hash ='".substr($hash,1,strlen($hash))."'");
+    $res->fetch_assoc();
+    header('Location: '.$res);
+} catch (\Throwable $th) {
+    echo <<<TEXT
+    <!DOCTYPE html>
+    <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+    <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+    <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+    <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="./style.css">
+    </head>
+    <body>
+    <h1>Dev-Lab 404</h1>
+    <div>Cette page n'existe pas</div>
+    </body>
+TEXT;
+}
+?>
