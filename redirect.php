9365d2ba15b2a15fc7a0ff32c5f98ca0398cb420
ErnestBidouille <theo.chennebault@gmail.com>
2019-05-29 00:58:06 +0200

    Vas y bg

--- redirect.php
+++ redirect.php
@@ -1,9 +1,12 @@
 <?php
 $hash =  "{$_SERVER['REQUEST_URI']}";
 try {
+    $conn = new mysqli("0.0.0.0:3306", "demouser", "demopassword", "shortnerdb");
     $res = $conn->query("Select url from shorttable where hash ='".substr($hash,1,strlen($hash))."'");
     $res->fetch_assoc();
     header('Location: '.$res);
+    mysqli_close($conn);
+    exit();
 } catch (\Throwable $th) {
     echo <<<TEXT
     <!DOCTYPE html>
